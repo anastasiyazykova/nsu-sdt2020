1e66af2f54dc4ae06239520c79f3069b1a18ef86
Anastasiya Zykova <azalekss@mail.ru>
2020-10-28 14:03:58 +0700

    4 done

--- clojure1.4/src/clojure1/4.clj
+++ clojure1.4/src/clojure1/4.clj
@@ -1,18 +1,20 @@
 (ns clojure1.4)
 
-(defn add_alphabet_to_word [started alphabet]
-  (filter (fn [it] (not= started (first it))) alphabet)
+(defn delete-repeat
+  [started comb]
+  (filter #(not= started (first %)) comb))
 
-(defn add_new_combo [started alphabet]
-  ()
+(defn add_alphabet_to_word
+  [started comb]
+  (map #(conj % started) (delete-repeat started comb)))
 
-(defn main_rec [started n alphabet]
-  (if (= n 0)
-    started
-    (main_rec (add_new_combo started alphabet) (- n 1) alphabet)))
+(defn add_new_combo
+  [alphabet comb]
+  (mapcat #(add_alphabet_to_word % comb) alphabet))
 
-(defn resolution [n alphabet]
-  (nth (iterate (partial add_new_combo alphabet) '(list)) n))
+(defn resolution
+  [n alphabet]
+  (nth (iterate #(add_new_combo alphabet %) '(())) n))
 
 (defn -main
   [& args]
