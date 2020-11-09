7658198f1f7d59d9054a75869f3a7ec4e7ebcb8f
Anastasiya Zykova <azalekss@mail.ru>
2020-11-09 17:23:16 +0700

    2, 2.1 fixed

--- clojure2/src/clojure2/core.clj
+++ clojure2/src/clojure2/core.clj
@@ -1,21 +1,27 @@
 (ns clojure2.core)
 
-(defn resolution [fun a b]
-  (* (- b a) (/ (+ (fun a) (fun b)) 2.0)))
+;; 2
+(defn trapezium-method [fun a b]
+  (* (- b a) (/ (+ (fun a) (fun b)) 2)))
 
 (def memo
   (memoize (fn [fun dt n]
-             (if (> n 0.0)
-              (+ (memo fun dt (- n 1)) (resolution fun (* dt (- n 1)) (* dt n)))
-             0.0))))
+             (if (> n 0)
+              (+ (memo fun dt (- n 1)) (trapezium-method fun (* dt (- n 1)) (* dt n)))
+             0))))
 
+;; 2.1
 (defn first-opt [fun dt x]
-  (if (> dt 0.0)
-    (let [n (/ x dt)] (+ (memo fun dt n) (resolution fun (* dt n) x)))
-    0.0))
+  (if (> dt 0)
+    (let [n (/ x dt)] (+ (memo fun dt n) (trapezium-method fun (* dt n) x)))
+    0))
+
+;; 2.2
+(defn second-opt [fun dt x]
+  )
 
 (defn -main []
 
-  (time (println (resolution (fn [x] x) 0.0 20.0)))
-
-  (time (println (first-opt (fn [x] x) 1.0 20.0))))
+  (time (println (float (trapezium-method (fn [x] x) 0 21))))
+  (time (println (float (first-opt (fn [x] x) 1 21))))
+  )
