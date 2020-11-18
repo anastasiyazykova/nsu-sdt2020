759ca9372e9d380135defd5a36c1271e471f0fdb
Anastasiya Zykova <azalekss@mail.ru>
2020-11-18 13:59:12 +0700

    2, 2.1, 2.2 done, delete floats, added tests

--- clojure2/src/clojure2/core.clj
+++ clojure2/src/clojure2/core.clj
@@ -1,4 +1,4 @@
-(ns clojure2.core)
+(ns clojure2.core (:use [clojure.test]))
 
 ;; 2
 (defn trapezium-method [fun a b]
@@ -12,16 +12,40 @@
 
 ;; 2.1
 (defn first-opt [fun dt x]
-  (if (> dt 0)
+  (if (not= dt 0)
     (let [n (/ x dt)] (+ (memo fun dt n) (trapezium-method fun (* dt n) x)))
     0))
 
 ;; 2.2
-(defn second-opt [fun dt x]
-  )
+(defn lazy [fun dt]
+  (map first (iterate (fn [[acc x]] [(+ (trapezium-method fun x (+ x dt)) acc) (+ x dt)]) [0 0])))
+
+(defn second-opt [fun dt]
+    (fn [x] (nth (lazy fun dt) (/ x dt))))
+
+(deftest trapezium-method-test
+  (is (= (trapezium-method (fn [x] x) 0 0)) 0)
+  (is (= (trapezium-method (fn [x] x) 0 10)) 50)
+  (is (= (trapezium-method (fn [x] x) 0 100)) 5000)
+  (is (= (trapezium-method (fn [x] x) 3 10)) 91/2)
+  (is (= (trapezium-method (fn [x] x) 4 15)) 209/2))
+
+(deftest first-opt-test
+  (is (= (first-opt (fn [x] x) 0 0)) 0)
+  (is (= (first-opt (fn [x] x) 0 10)) 0)
+  (is (= (first-opt (fn [x] x) 1 100)) 5000)
+  (is (= (first-opt (fn [x] x) 2 100)) 5000)
+  (is (= (first-opt (fn [x] x) 2/10 13)) 169/2))
+
+(deftest second-opt-test
+  (is (= ((second-opt (fn [x] x) 1) 100)) 5000)
+  (is (= ((second-opt (fn [x] x) 2) 100)) 5000)
+  (is (= ((second-opt (fn [x] x) 2/10) 13)) 169/2))
 
 (defn -main []
-
-  (time (println (float (trapezium-method (fn [x] x) 0 21))))
-  (time (println (float (first-opt (fn [x] x) 1 21))))
-  )
+  (time (float (trapezium-method (fn [x] x) 0 21)))
+  (time (float (first-opt (fn [x] x) 1 21)))
+  (time (float ((second-opt (fn [x] x) 1) 10)))
+  (trapezium-method-test)
+  (first-opt-test)
+  (second-opt-test))
