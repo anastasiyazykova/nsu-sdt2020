7a5584ab1af4b00d73521017a7e8137f30a0dad7
Anastasiya Zykova <azalekss@mail.ru>
2021-01-10 15:00:35 +0700

    rules, done

--- clojure4/src/clojure4/core.clj
+++ clojure4/src/clojure4/core.clj
@@ -36,7 +36,8 @@
 
 (defn invert-value [expr]
   {:pre [(invert? expr)]}
-  (second expr))
+  (second expr)
+  )
 
 ; Conjunct
 
@@ -54,6 +55,13 @@
 (defn disjunct? [expr]
   (= (first expr) ::disj))
 
+(defn parts [expr]
+  {:pre [(or
+           (conjunct? expr)
+           (disjunct? expr)
+           )]}
+  (rest expr))
+
 ; Implicat
 
 (defn implicat [expr_left expr_right]
@@ -70,16 +78,108 @@
   {:pre [(implicat? expr)]}
   (nth expr 2))
 
+(defn id [_] _)
+
+(declare dnf)
+
 (def dnf-rules
-  (
-    
-    ))
+  (list
+    ; Constant
+    [constant? id]
+    ; Variable
+    [variable? id]
+    ; Invert
+    [invert?
+     (fn [expr]
+       (let [subexpr (invert-value expr)]
+         (cond
+           (constant? subexpr) expr
+           (variable? subexpr) expr
+           (invert? subexpr) (invert-value (invert-value subexpr))
+           ; Invert conj/disj
+           (disjunct? subexpr) (dnf
+                                 (apply conjunct (map
+                                                   #(dnf (invert %))
+                                                   (parts subexpr))))
+
+           (conjunct? subexpr) (dnf
+                                 (apply disjunct (map
+                                                   #(dnf (invert %))
+                                                   (parts subexpr))))
+           ; ¬ (A => B)  =  ¬(¬A or B) = (A and ¬B)
+           (implicat? subexpr) (dnf
+                                 (conjunct
+                                   (left subexpr)
+                                   (invert (right subexpr)))))))]
+    ; Implicat
+    [implicat? (fn [expr]
+                 (dnf (disjunct
+                        (invert (left expr))
+                        (right expr))))]
+    ; Disjunct
+    [disjunct?
+     (fn [expr]
+       (apply disjunct (mapcat #(if (disjunct? %)           ; A or (B or C) = A or B or C
+                                  (map (fn [arg] (dnf arg)) (parts %))
+                                  (list (dnf %)))
+                               (parts expr))))]
+    [conjunct?
+     (fn [expr]
+       (let [flattened (mapcat #(if (conjunct? %)
+                                  (map (fn [arg] (dnf arg)) (parts %))
+                                  (list (dnf %)))
+                               (parts expr))
+             found_disjunction (first (filter (fn [arg] (disjunct? arg)) flattened))]
+         (if (nil? found_disjunction)
+           flattened
+           ; A and (B or C) = A and B or A and C
+           (apply disjunct
+                  (map
+                    (fn [arg]
+                      (apply conjunct (cons arg (filter
+                                                  (fn [conjunction] (not (identical? conjunction found_disjunction)))
+                                                  flattened
+                                                  ))))
+                    (parts found_disjunction))))))]))
 
 (defn dnf [expr]
   ((some
      (fn [rule]
-           (if ((first rule) expr)
-             (second rule)
-             false))
+       (if ((first rule) expr)
+         (second rule)
+         false))
      dnf-rules)
    expr))
+
+(defn expr_type [expr] (first expr))
+
+(defn check_eq [expr1 expr2]
+  (if (=
+        (expr_type expr1)
+        (expr_type expr2))
+
+    (cond
+      (constant? expr1) (=
+                          (constant-value expr1)
+                          (constant-value expr2))
+      (variable? expr1) (=
+                          (variable-value expr1)
+                          (variable-value expr2))
+      (invert? expr1) (check_eq
+                        (invert-value expr1)
+                        (invert-value expr2))
+      (implicat? expr1) (and
+                          (check_eq
+                            (left expr1)
+                            (left expr2))
+                          (check_eq
+                            (left expr1)
+                            (left expr2)))
+      (or
+        (disjunct? expr1)
+        (conjunct? expr1)) (every?
+                             #(check_eq (first %) (second %))
+                             (map vector
+                                  (parts expr1)
+                                  (parts expr2))))
+    false))
--- clojure4/test/clojure4/core_test.clj
+++ clojure4/test/clojure4/core_test.clj
@@ -1,7 +1,14 @@
 (ns clojure4.core-test
-  (:require [clojure.test :refer :all]
-            [clojure4.core :refer :all]))
+  (:require [clojure.test :as test]
+            [clojure4.core :as dnfp]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(test/deftest dnf-test
+  (let [x (dnfp/variable :x)
+        y (dnfp/variable :y)
+        z (dnfp/variable :z)]
+
+    (test/testing
+      (test/is (dnfp/check_eq (dnfp/dnf (dnfp/conjunct (dnfp/implicat x y) z))
+                  (dnfp/disjunct (dnfp/conjunct (dnfp/invert x) z) (dnfp/conjunct y z)))))))
+
+(test/run-tests 'clojure4.core-test)
